d7d3867370dd9e87734a3c6b2cf4121c7f5cc50c
Felipe Rostirolla <felipe.rost.f@gmail.com>
2024-09-11 15:56:58 -0300

    selecting and submitting selected words basic functionality

--- app/components/WordBox.tsx
+++ app/components/WordBox.tsx
@@ -4,25 +4,33 @@ import { useState } from "react";
 export type WordProps = { 
   word: string; 
   group: string;
+  toggleWordSelection: (word: string, group: string, isSelected: boolean) => void; 
+  // toggle function coming through the parent /\
 };
 
-const WordBox = ({ word, group }: WordProps) => {
+const WordBox = ({ word, group, toggleWordSelection }: WordProps) => {
 
-  const [selected, setSelected] = useState(false)
+  const [selected, setSelected] = useState(false);
+
+  const handleClick = () => {
+    setSelected(!selected);
+
+    // on click, adds or removes the word & group from the list of selected elements.
+    toggleWordSelection(word, group, selected); 
+    
+  };
 
   return (
     <>
-      <button className={selected 
-      ? "font-sans font-bold uppercase w-[150px] h-[80px] rounded-md border-solid border-2 p-2 text-center content-center bg-slate-400 hover:bg-slate-600 active:bg-slate-400"
-      : "font-sans font-bold uppercase w-[150px] h-[80px] rounded-md border-solid border-2 p-2 text-center content-center bg-slate-100 hover:bg-slate-200 active:bg-slate-400"} 
-      id={group}
-        onClick={() => {
-          setSelected(!selected)
-          console.log(selected)
-          console.log(word)
-        }}>
-            {word}
-        </button>
+      <button
+        className={selected 
+          ? "font-sans font-bold uppercase w-[150px] h-[80px] rounded-md border-solid border-2 p-2 text-center content-center bg-slate-400 hover:bg-slate-600 active:bg-slate-400"
+          : "font-sans font-bold uppercase w-[150px] h-[80px] rounded-md border-solid border-2 p-2 text-center content-center bg-slate-100 hover:bg-slate-200 active:bg-slate-400"}
+        id={group}
+        onClick={handleClick}
+      >
+        {word}
+      </button>
     </>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,19 @@ import { useState } from "react";
 export default function Home() {
   const { words } = useLogic()
 
-  const [selectedWords, setSelectedWords] = useState([]);
+  // global state of selected words
+  const [selectedWords, setSelectedWords] = useState<{ word: string, group: string }[]>([]); 
+
+  // function to add and remove word&group from an array when user clicks the word button
+  const toggleWordSelection = (word: string, group: string, isSelected: boolean) => {
+    if (isSelected) {
+      // if button state is selected and the user clicks it, it means the user wants to remove the clicked word from the list. 
+      setSelectedWords(prevWords => prevWords.filter(selectedWord => selectedWord.word !== word));
+    } else {
+      // if button state is not selected (which means the user now wants to select and add the word), adds the word to selected words list.
+      setSelectedWords(prevWords => [...prevWords, { word, group }]);
+    }
+  };
 
   const handleSubmit = () => {
     console.log('selected words:', selectedWords);
@@ -24,8 +36,10 @@ export default function Home() {
         <ul className="grid grid-cols-4 gap-4">
           {words?.map((word, group) => (
             <li key={group}>
-              <WordBox {...word} />
-
+              <WordBox {...word} 
+              toggleWordSelection={toggleWordSelection}
+              />
+              
             </li>
           ))}
         </ul>
@@ -37,7 +51,8 @@ export default function Home() {
 
       <button>Shuffle</button>
       <button>Deselect all</button>
-      <button onClick={handleSubmit}>Submit</button>
+      <button className="font-sans font-bold uppercase w-[150px] h-[80px] rounded-md border-solid border-2 p-2 text-center content-center bg-slate-400 hover:bg-slate-600 active:bg-slate-400" 
+      onClick={handleSubmit}>Submit</button>
 
     </main>
   );
