3d7d8e941146f9e22fe30c5c39ee8aeb9c802644
Felipe Rostirolla <felipe.rost.f@gmail.com>
2024-10-24 16:28:11 -0300

    style: added fade in effect for modal

--- app/components/HelpModal.tsx
+++ app/components/HelpModal.tsx
@@ -14,7 +14,7 @@ const HelpModal = ({ closeHelp }: HelpModalProps)  => {
       };
 
   return (
-    <div className="transition-opacity duration-300">
+    <div className="fade-in">
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" onClick={handleClickOutside}>
             <div className="bg-white p-8 rounded shadow-lg max-w-lg w-full relative">
             <h2 className="text-2xl font-bold mb-4">How to play</h2>
@@ -23,7 +23,7 @@ const HelpModal = ({ closeHelp }: HelpModalProps)  => {
             <p className="mb-2">• Click the words to select them, and click <b>Submit</b> to check if your guess is correct.</p>
             <p className="mb-2">• You can make 4 mistakes before the game is over.</p>
             <p className="mb-2">• For development purposes, the words and groups may be very easy to identify.</p>
-            <button onClick={closeHelp} className="p-2 bg-red-500 hover:bg-red-400 transition text-white rounded absolute top-2 right-2">Close</button>
+            <button onClick={closeHelp} className="p-2 bg-red-500 hover:bg-red-400 text-white rounded absolute top-2 right-2">Close</button>
             </div>
         </div>
     </div>
